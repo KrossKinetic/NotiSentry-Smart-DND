c855f5512d11fff5a5a71c6e5af76473d222ddc1
krosskinetic <anshumaanvikramsinghlucknow@gmail.com>
2025-07-28 03:14:25 -0400

    Updated Versioning
    Signed-off-by: krosskinetic <anshumaanvikramsinghlucknow@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.krosskinetic.notisentry"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "Alpha-1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
